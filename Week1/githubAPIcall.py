725a7928e07fa36902a769b9a7b1e6e7d328eeb1
codesbybilal <bilalathar416@gmail.com>
2026-09-19 10:36:45 +0500

    Fix typo in error handling (response.status_code)

--- Week1/githubAPIcall.py
+++ Week1/githubAPIcall.py
@@ -1,7 +1,13 @@
 import requests
 response = requests.get("https://api.github.com/users/codesbybilal")
-data = response.json()
-print("Github Info of @codesbybilal")
-print("Name: ", data["name"])
-print("Bio: ", data["bio"])
-print("Followers: ", data["followers"])
+if response.status_code == 200:
+    data = response.json()
+    print(response.status_code)
+    print("Github Info of @codesbybilal")
+    print("Name: ", data["name"])
+    print("Bio: ", data["bio"])
+    print("Followers: ", data["followers"])
+elif response.status_code == 404:
+    print("User not found.")
+else:
+    print(f"Something went wrong. Error {response.status_code}")
